f4b1014297d06c23324e72fc70146c17d07ea9b2
Aga <aga.k396@gmail.com>
2020-05-27 12:07:38 +0200

    Create all inputs for createtin new Ability

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {
   StyleSheet,
   ScrollView,
   View,
   Button,
   Text,
+  TextInput,
 } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import Modal from 'react-native-modal';
 import Abilities from './Abilities.js';
-
-const STORAGE_KEY ='@save_state'
+import RadioButton from 'react-native-paper/lib/commonjs/components/RadioButton/RadioButton';
 
 export default class App extends Component {
   state = {
@@ -76,7 +76,6 @@ export default class App extends Component {
   };
 
   longRest() {
-    // alert('long rest!');
     let abilities = this.state.abilities;
     for (let i=0; i<abilities.length; i++){
       abilities[i].usedSlots = 0
@@ -86,7 +85,6 @@ export default class App extends Component {
   }
 
   shortRest() {
-    // alert('short rest!');
     let abilities = this.state.abilities;
     for (let i=0; i<abilities.length; i++){
       if(abilities[i].shortRest){
@@ -97,31 +95,14 @@ export default class App extends Component {
     this.saveState()
   }
 
-  addAbility(props) {
-    const modalVisible = this.state.modalVisible
-  
-    return(
-      <View>
-        <Modal 
-          isVisible={modalVisible}
-          onRequestClose={() => this.setState({modalVisible: false})}
-        >
-          <View style={styles.modalContainer}>
-            <Text>I am the modal content!</Text>
-            <Button 
-              onPress={() => this.setState({modalVisible: false})}
-              title="anuluj"
-            />
-          </View>
-        </Modal>
-      </View>
-    );
-  }
-
   render() {  
     return (
       <ScrollView style={styles.appView}>
-        {this.addAbility()}
+        <AddAbility 
+          modalVisible={this.state.modalVisible}
+          closeModal={() => this.setModalVisible(false)}
+        />
+        {/* {this.addAbility()} */}
         <Text>modal visible? {this.state.modalVisible ? "yes" : "no"}</Text>
         <View style={styles.controllerMenu}>
           <Button 
@@ -147,6 +128,67 @@ export default class App extends Component {
   }
 }
 
+function AddAbility(props) {
+  const modalVisible = props.modalVisible;
+  const [name, setName] = useState('');
+  const [maxSlots, setMaxSlots] = useState(0);
+  const [shortRest, setIfShortRest] = useState(false);
+  const [error, addError] = useState("");
+
+  return(
+    <View>
+      <Modal 
+        isVisible={modalVisible}
+        onRequestClose={() => props.closeModal()}
+      >
+        <View style={styles.modalContainer}>
+          <Text>Dodaj nową umiejętność</Text>
+          <TextInput
+            placeholder="nazwa"
+            placeholderTextColor="rgba(0,0,0,0.5)"
+            maxLength={40}
+            onChangeText={text => setName(text)}
+          />
+          <TextInput
+            placeholder="liczba użyć"
+            placeholderTextColor="rgba(0,0,0,0.5)"
+            maxLength={1}
+            keyboardType={'numeric'}
+            onChangeText={number => 
+              setMaxSlots(number.replace(/[^1-9]/g, ''))
+            }
+          />
+          <View style={styles.radioButton}>
+            <Text style={{fontSize: 25}}>Short rest</Text>
+            <RadioButton
+              value="Short rest"
+              status={shortRest === true ? 'checked' : 'unchecked'}
+              onPress={() => setIfShortRest(true)}
+            />
+          </View>
+          <View style={styles.radioButton}>
+            <Text style={{fontSize: 25}}>Long rest</Text>
+            <RadioButton
+              value="Long rest"
+              status={shortRest === false ? 'checked' : 'unchecked'}
+              onPress={() => setIfShortRest(false)}
+            />
+          </View>
+          <Text>nowa umiejętność:</Text>
+          <Text>nazwa: {name}</Text>
+          <Text>liczba użyć: {maxSlots}</Text>
+          <Text>rodzaj: {shortRest ? "short rest" : "long rest"}</Text>
+          <Text>{error}</Text>
+          <Button 
+            onPress={() => props.closeModal()}
+            title="anuluj"
+          />
+        </View>
+      </Modal>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   appView: {
     backgroundColor: '#fff',
@@ -160,5 +202,11 @@ const styles = StyleSheet.create({
   modalContainer: {
     backgroundColor: '#fff',
     padding: 30,
+  },
+  radioButton: {
+    flexWrap: 'wrap', 
+    flexDirection: 'row',
+    alignContent: 'flex-end'
+    // justifyContent: 'center'
   }
 });
