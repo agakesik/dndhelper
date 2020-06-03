16327a507fcec52c507620c7de6010d036e488bf
Aga <aga.k396@gmail.com>
2020-06-03 13:44:22 +0200

    Style modal for menaging and editing abilities

--- Modals.js
+++ Modals.js
@@ -1,9 +1,10 @@
 
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import Modal from 'react-native-modal';
 import { RadioButton, TextInput, Text, Subheading, Button, Headline } from 'react-native-paper'
 import { theme } from './Styles.js'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export function AddAbility(props) {
   const [name, setName] = useState('');
@@ -90,7 +91,7 @@ export function AddAbility(props) {
             dodaj
           </Button>
           <Button 
-            mode="contained"
+            // mode="contained"
             style={styles.button}
             onPress={() => props.closeModal()}
           >
@@ -120,50 +121,75 @@ export function ManageAbilities(props) {
         onRequestClose={() => props.closeModal()}
       >
         <View style={styles.modalContainer}>
-          {abilities.map((ability, i) => (
-            <View key={i}
-            style={{flexDirection: 'row', justifyContent: "flex-end",}}
-            >
-              <RadioButton.Item
-                key={i}
-                label={ability.name +": "+ ability.usedSlots +"/" +ability.maxSlots}
-                value={i}
-                status={abilityNumber === i ? 'checked' : 'unchecked'}
-                onPress={() => chooseAbility(i)}
-              />
-              <Button 
-                onPress={() => props.moveUp(i)}
-                title="^"
-                disabled={i===0 ? true : false}
-              />
-              <Button 
-                onPress={() => props.moveDown(i)}
-                title="v"
-                disabled={i===abilities.length-1 ? true : false}
-              />
-            </View>
-          ))}
-          <Button 
-            title="usuń"
-            onPress={() => {
-              props.deleteAbility(abilityNumber)
-              chooseAbility(null)
-            }}
-          />
-          <Button 
-            title="edytuj"
-            onPress={() => {
-              if (abilityNumber || abilityNumber === 0) {
-                props.openEdit()}
+          <ScrollView style={styles.abilitiesList}>
+            {abilities.map((ability, i) => (
+              <View key={i}
+              style={styles.abilityRow}
+              >
+                <RadioButton.Item
+                  key={i}
+                  label={ability.name +": "+ ability.usedSlots +"/" +ability.maxSlots}
+                  value={i}
+                  status={abilityNumber === i ? 'checked' : 'unchecked'}
+                  onPress={() => chooseAbility(i)}
+                  color={theme.colors.primary}
+                />
+                <View>
+                  <Button 
+                    onPress={() => props.moveUp(i)}
+                    disabled={i===0 ? true : false}
+                    style={styles.togglePositionButton}
+                    mode="contained"
+                  >
+                    <Icon name='arrow-up' />
+                  </Button>
+                  <Button 
+                    onPress={() => props.moveDown(i)}
+                    disabled={i===abilities.length-1 ? true : false}
+                    style={styles.togglePositionButton}
+                    mode="contained"
+                  >
+                    <Icon name='arrow-down' style={{width: 100}}/>
+                  </Button>
+                </View>
+              </View>
+            ))}
+          </ScrollView>
+          <View style={[styles.controllerMenu, {backgroundColor: theme.colors.primary, marginTop: 10, marginBottom: 5}]}>
+            <Button 
+              title="usuń"
+              onPress={() => {
+                props.deleteAbility(abilityNumber)
+                chooseAbility(null)
               }}
-          />
+              color={theme.colors.background}
+              style={styles.controllerButton}
+
+            >
+              usuń
+            </Button>
+            <Button 
+              title="edytuj"
+              onPress={() => {
+                if (abilityNumber || abilityNumber === 0) {
+                  props.openEdit()}
+                }}
+                color={theme.colors.background}
+                style={styles.controllerButton}
+
+            >
+              edytuj
+            </Button>
+          </View>
           <Button 
             onPress={() => {
               props.closeModal()
               chooseAbility(null)
             }}
-            title="zamknij"
-          />
+            style={styles.controllerButton}
+          >
+            zamknij
+          </Button>
         </View>
       </Modal>
     </View>
@@ -184,7 +210,7 @@ export function EditAbility(props) {
         onRequestClose={() => props.closeModal()}
       >
         <View style={styles.modalContainer}>
-          <Text>Edytuj {ability.name}</Text>
+          <Headline style={{marginBottom: 15}}>Edytuj {ability.name}</Headline>
 
           <TextInput
             placeholder={"nazwa: " + ability.name}
@@ -196,24 +222,28 @@ export function EditAbility(props) {
            value={maxSlots}
            onChangeText={text => setMaxSlots(text)}
           />
-          <RadioButton.Group
-            onValueChange={value => setIfShortRest(value)}
-          >
-            <View style={styles.radioButton}>
-              <RadioButton.Item 
-                label={(ability.shortRest ? "-> " : "    ") + "short rest"}
-                value={true}
-                status={isShortRest === true ? 'checked' : 'unchecked'}
-                />
-            </View>
-            <View style={styles.radioButton}>
-              <RadioButton.Item
-                label={(ability.shortRest ? "    " : "-> ") + "long rest"}
-                value={false}
-                status={isShortRest === false ? 'checked' : 'unchecked'}
-                />
-            </View>
-          </RadioButton.Group>
+          <View style={styles.radioButtonContainer}>
+            <RadioButton.Group
+              onValueChange={value => setIfShortRest(value)}
+            >
+              <View style={styles.radioButton}>
+                <RadioButton.Item 
+                  label={(ability.shortRest ? "-> " : "    ") + "short rest"}
+                  value={true}
+                  status={isShortRest === true ? 'checked' : 'unchecked'}
+                  color={theme.colors.primary}
+                  />
+              </View>
+              <View style={styles.radioButton}>
+                <RadioButton.Item
+                  label={(ability.shortRest ? "    " : "-> ") + "long rest"}
+                  value={false}
+                  status={isShortRest === false ? 'checked' : 'unchecked'}
+                  color={theme.colors.primary}
+                  />
+              </View>
+            </RadioButton.Group>
+          </View>
           <Button 
             onPress={() => {
               props.editAbility(name, maxSlots, isShortRest)
@@ -222,12 +252,16 @@ export function EditAbility(props) {
               setMaxSlots(null);
               setIfShortRest(null);
             }}
-            title="edytuj"
-          />
+            style={{marginTop: 10}}
+            mode="contained"
+          >
+            edytuj
+          </Button>
           <Button 
             onPress={() => {props.closeModal()}}
-            title="anuluj"
-          />
+          >
+            anuluj
+          </Button>
         </View>
       </Modal>
     </View>
@@ -237,7 +271,20 @@ export function EditAbility(props) {
 const styles = StyleSheet.create({
   modalContainer: {
     backgroundColor: '#fff',
-    padding: 30,
+    padding: 5,
+    maxHeight: '90%'
+  },
+  controllerMenu: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    padding: 5,
+  },
+  controllerButton: {
+    minWidth: '50%', 
+    borderRadius: 20,
+  },
+  abilitiesList: {
+    padding: 10,
   },
   radioButtonContainer: {
     // backgroundColor: 'red',
@@ -258,5 +305,11 @@ const styles = StyleSheet.create({
   },
   description: {
     color: theme.colors.gray,
-  }
+  },
+  abilityRow: {
+    flexDirection: 'row', 
+    justifyContent: "flex-end",
+    alignItems: 'center',
+    padding: 5,
+  },
 });
